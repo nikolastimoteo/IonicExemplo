0cf40b02fa11d79488e10ed4d9105a6e6011f7ca
unknown <nikolastps@hotmail.com>
2018-04-02 19:44:32 -0300

    Finish SettingsPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,20 +14,30 @@ export class HomePage {
     city:string,
     state:string
   }
-  constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {
+  constructor(public navCtrl: NavController,
+    private weatherProvider:WeatherProvider,
+    private storage:Storage) {
 
   }
 
   ionViewWillEnter(){
-    this.location = {
-      city: 'Miami',
-      state: 'FL'
-    }
 
-    this.weatherProvider.getWeather(this.location.city, this.location.state)
-    .subscribe(weather =>{
-      //console.log(weather);
-      this.weather = weather.current_observation;
+    this.storage.get('location').then((val) => {
+      if(val != null){
+        this.location = JSON.parse(val);
+      } else {
+        this.location = {
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+      this.weatherProvider.getWeather(this.location.city, this.location.state)
+      .subscribe(weather =>{
+        //console.log(weather);
+        this.weather = weather.current_observation;
+      });
     });
+
+    
   }
 }
